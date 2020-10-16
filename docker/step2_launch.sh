a79907a490857540b6323995042ccaf412e004c7
Taylor Dahlke <taylor@hivemapper.com>
2020-10-16 14:46:56 -0700

    Update step2_launch.sh
    Fixed bug in launch script.

--- docker/step2_launch.sh
+++ docker/step2_launch.sh
@@ -1,4 +1,4 @@
-docker run -w /working -v $PWD/../:/working -it online_photometric_calibration:latest
+docker run -w /working -v $1/../:/working -it online_photometric_calibration:latest
 echo "=================================================================="
 echo " USAGE: "
 echo "./step2_launch.sh /PathToMyLocalDir/MyVideos "
